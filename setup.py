405e200895229715fb338026fd2e897ed1545f6a
Asif bin Syed <Asifbinsyed33@gmail.com>
2024-07-05 22:50:53 -0400

    requirements.txt update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Asif",
+    author_email="asifbinsyed33@gmail.com",
+    packages=find_packages()
+)
